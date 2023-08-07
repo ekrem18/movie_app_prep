79f08bcd5b0ca0e631fa913e69628a395f7d8a11
Ekrem <avekremyilmazturk@gmail.com>
2023-08-07 15:59:36 +0300

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
+ 
   theme: {
     
     extend: {
