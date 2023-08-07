331110466734f1e5e1b9c7df1fa1671f7e512903
Ekrem <avekremyilmazturk@gmail.com>
2023-08-07 14:42:21 +0300

    this thime i hope

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-export default function App() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
+import React from "react";
+import Register from "./pages/Register";
+
+const App = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default App;
