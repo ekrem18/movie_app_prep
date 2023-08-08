998715ac6d871f41c5b4dcd147c13a53b59fd25c
Ekrem <avekremyilmazturk@gmail.com>
2023-08-08 23:00:15 +0300

    navigate done

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,19 @@
 import React, { createContext } from "react";
 import { auth } from "../auth/firebase";
 import {  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
+
 const AuthContextProvider = ({ children }) => {
+    let navigate = useNavigate() 
   const createUser = async (email, password) => {
     // yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
     try {
       let userCredential = await createUserWithEmailAndPassword(auth, email, password);
       console.log(userCredential);
+      navigate("/")
     } catch (error) {
       console.log(error);
     }
@@ -20,6 +24,7 @@ const AuthContextProvider = ({ children }) => {
     try {
      let userCredential =  await signInWithEmailAndPassword(auth, email, password)
      console.log(userCredential);
+     navigate("/")
     } catch (error) {
         
     }
