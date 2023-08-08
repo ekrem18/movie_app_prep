a573a587c749c4d399561054afadf0aeff11456b
Ekrem <avekremyilmazturk@gmail.com>
2023-08-08 22:09:40 +0300

    context system

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,12 +1,22 @@
-import React, { createContext } from 'react'
+import React, { createContext } from "react";
+import { auth } from "../auth/firebase";
+import {  createUserWithEmailAndPassword } from "firebase/auth";
 
-const AuthContext = createContext()
+export const AuthContext = createContext();
 
-const AuthContextProvider = ({children}) => {
-  return (
-    <AuthContext.Provider value={null}>{children}</AuthContext.Provider>
-  )
-}
+const AuthContextProvider = ({ children }) => {
+  const createUser = async (email, password) => {
+    // yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+    const values={createUser}
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
 //children In gelebilmesi için  AuthContext.Provider  ile sarmallama yapmamız gerekiyor. App js içierisnde bütün yapıyı kapsayan AppRouter ı kapsadığımız için children approuter olmuş oldu
 
-export default AuthContextProvider
+export default AuthContextProvider;
