f30e253e19d2b05fb91dadd68c7918739590831b
Ekrem <avekremyilmazturk@gmail.com>
2023-08-08 21:01:04 +0300

    env ready

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -4,7 +4,12 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  // ...
+    apiKey: "AIzaSyBa2VLZPPitO1y3UlBzYCZDUMbwqKX3cIw",
+    authDomain: "movie-app-last.firebaseapp.com",
+    projectId: "movie-app-last",
+    storageBucket: "movie-app-last.appspot.com",
+    messagingSenderId: "349540155905",
+    appId: "1:349540155905:web:82a071e96eca14e8a8b600"
 };
 
 // Initialize Firebase
