992932aff0facaa7c7bd70e84196a9975bd8f91e
Ekrem <avekremyilmazturk@gmail.com>
2023-08-09 18:40:19 +0300

    videosect added

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-// import VideoSection from "../components/VideoSection";
+import VideoSection from "../components/VideoSection";
 
 const MovieDetail = () => {
   const {id} = useParams();
